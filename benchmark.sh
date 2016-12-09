11fa02627f1d7ae2ee8435510d1fda31b6d36bab
xplicit <svg@ngs.ru>
2016-12-09 20:58:15 +0700

    Make benchmark output more easily to read

--- benchmark.sh
+++ benchmark.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install -y wrk
+sudo apt-get update > /dev/null
+sudo apt-get install -y wrk > /dev/null
 
 #benchmarking
 #warm up
 wrk -c 256 -t 8 -d 2 http://$1:5050/json/reply/hello?name=world
 #benchmarking
-wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world
-wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world
-wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world
-wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world
-wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world
+echo "Benchmarking .NET Core /json/reply/hello?name=world"
+wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world | grep Requests
+wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world | grep Requests
+wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world | grep Requests
+wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world | grep Requests
+wrk -c 256 -t 8 -d 30 http://$1:5050/json/reply/hello?name=world | grep Requests
+
