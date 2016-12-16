ed60b7c71c0b0689b0e8316c82926aa57e953902
xplicit <svg@ngs.ru>
2016-12-16 17:23:59 +0700

    Fix build script of netcore app

--- deploy-ss.sh
+++ deploy-ss.sh
@@ -8,10 +8,11 @@ sudo apt-get install -y dotnet-dev-1.0.0-preview2.1-003177
 mkdir -p /var/www/netcore && chown -R www-data:www-data /var/www
 su -c "cd /var/www/netcore && git clone https://github.com/NetCoreApps/Benchmarking" -s /bin/sh www-data
 
-cd /var/www/netcore/Benchmarking/src/WebApp
+cd /var/www/netcore/Benchmarking/src
 ls -al
 HOME=/root dotnet restore
 echo "restored"
+cd /var/www/netcore/Benchmarking/src/WebApp
 HOME=/root dotnet build -c Release
 echo "Built"
 HOME=/root nohup dotnet run -c Release > /dev/null 2>&1 & echo $! > hello.pid
