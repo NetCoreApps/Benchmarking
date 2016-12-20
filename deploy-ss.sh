ac9eb20e54f9882f050a8bfab1be18a7a8eb3fb5
Sergey Zhukov <svg@ngs.ru>
2016-12-20 07:36:24 +0700

    Benchmark profiler branch

--- deploy-ss.sh
+++ deploy-ss.sh
@@ -6,7 +6,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 sudo apt-get update
 sudo apt-get install -y dotnet-dev-1.0.0-preview2.1-003177
 mkdir -p /var/www/netcore && chown -R www-data:www-data /var/www
-su -c "cd /var/www/netcore && git clone https://github.com/NetCoreApps/Benchmarking" -s /bin/sh www-data
+su -c "cd /var/www/netcore && git clone -b profiler https://github.com/NetCoreApps/Benchmarking" -s /bin/sh www-data
 
 cd /var/www/netcore/Benchmarking/src
 ls -al
