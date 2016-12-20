f7d0c59973a527c204f06140a221c28b8d88cb68
xplicit <svg@ngs.ru>
2016-12-20 09:52:54 +0700

    Change branch to profiler for scripts

--- azure-benchmark.bat
+++ azure-benchmark.bat
@@ -16,15 +16,15 @@ for /f "tokens=3 delims=:" %%i in ('azure network nic show -g benchmark nic-benc
 for /f "tokens=1" %%i in ("%INTERNAL_IP%") do SET INTERNAL_IP=%%i
 
 echo "Deploying .NET Core app"
-call azure vm extension set -g benchmark --vm-name benchmarkVM1 -n CustomScript --publisher-name Microsoft.Azure.Extensions --version 2.0 --public-config "{""fileUris"": [""https://raw.githubusercontent.com/NetCoreApps/Benchmarking/master/deploy-ss.sh""],""commandToExecute"": ""./deploy-ss.sh""}"
+call azure vm extension set -g benchmark --vm-name benchmarkVM1 -n CustomScript --publisher-name Microsoft.Azure.Extensions --version 2.0 --public-config "{""fileUris"": [""https://raw.githubusercontent.com/NetCoreApps/Benchmarking/profiler/deploy-ss.sh""],""commandToExecute"": ""./deploy-ss.sh""}"
 echo "Deploying mono app"
-call azure vm extension set -g benchmark --vm-name benchmarkVM1 -n CustomScript --publisher-name Microsoft.Azure.Extensions --version 2.0 --public-config "{""fileUris"": [""https://raw.githubusercontent.com/NetCoreApps/Benchmarking/master/deploy-mono.sh""],""commandToExecute"": ""./deploy-mono.sh %INTERNAL_IP%""}"
+call azure vm extension set -g benchmark --vm-name benchmarkVM1 -n CustomScript --publisher-name Microsoft.Azure.Extensions --version 2.0 --public-config "{""fileUris"": [""https://raw.githubusercontent.com/NetCoreApps/Benchmarking/profiler/deploy-mono.sh""],""commandToExecute"": ""./deploy-mono.sh %INTERNAL_IP%""}"
 
 REM Info about script installation can found at `/var/lib/waagent/custom-script/download/0/` in benchmarkVM1 machine
 REM Run benchmarks
 
 echo "Starting Benchmarking..."
-call azure vm extension set -g benchmark --vm-name benchmarkVM2 -n CustomScriptForLinux --publisher-name Microsoft.OSTCExtensions --version 1.5 --public-config "{""fileUris"": [""https://raw.githubusercontent.com/NetCoreApps/Benchmarking/master/benchmark.sh?v1""],""commandToExecute"": ""./benchmark.sh %INTERNAL_IP%""}"
+call azure vm extension set -g benchmark --vm-name benchmarkVM2 -n CustomScriptForLinux --publisher-name Microsoft.OSTCExtensions --version 1.5 --public-config "{""fileUris"": [""https://raw.githubusercontent.com/NetCoreApps/Benchmarking/profiler/benchmark.sh?v1""],""commandToExecute"": ""./benchmark.sh %INTERNAL_IP%""}"
 
 @echo off
 setlocal EnableDelayedExpansion
--- azure-benchmark.sh
+++ azure-benchmark.sh
@@ -13,15 +13,15 @@ az vm create --image canonical:UbuntuServer:16.04.0-LTS:latest --size ${AZ_SIZE}
 
 INTERNAL_IP=$(az vm list-ip-addresses --name BenchmarkVM1 | jq .[0].virtualMachine.network.privateIpAddresses[0] | sed -e 's/^"//' -e 's/"$//')
 echo "Deploying .NET Core app"
-az vm extension set -g benchmark --vm-name benchmarkVM1 -n CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --settings '{"fileUris": ["https://raw.githubusercontent.com/NetCoreApps/Benchmarking/master/deploy-ss.sh"],"commandToExecute": "./deploy-ss.sh"}'
+az vm extension set -g benchmark --vm-name benchmarkVM1 -n CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --settings '{"fileUris": ["https://raw.githubusercontent.com/NetCoreApps/Benchmarking/profiler/deploy-ss.sh"],"commandToExecute": "./deploy-ss.sh"}'
 echo "Deploying mono app"
-az vm extension set -g benchmark --vm-name benchmarkVM1 -n CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --settings '{"fileUris": ["https://raw.githubusercontent.com/NetCoreApps/Benchmarking/master/deploy-mono.sh"],"commandToExecute": "./deploy-mono.sh '"${INTERNAL_IP}"'"}'
+az vm extension set -g benchmark --vm-name benchmarkVM1 -n CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --settings '{"fileUris": ["https://raw.githubusercontent.com/NetCoreApps/Benchmarking/profiler/deploy-mono.sh"],"commandToExecute": "./deploy-mono.sh '"${INTERNAL_IP}"'"}'
 
 #Info about script installation can found at `/var/lib/waagent/custom-script/download/0/` in benchmarkVM1 machine
 #Run benchmarks
 
 echo "Starting Benchmarking..."
-az vm extension set -g benchmark --vm-name benchmarkVM2 -n CustomScriptForLinux --publisher Microsoft.OSTCExtensions --version 1.5 --settings '{"fileUris": ["https://raw.githubusercontent.com/NetCoreApps/Benchmarking/master/benchmark.sh?v1"],"commandToExecute": "./benchmark.sh '"${INTERNAL_IP}"'"}'
+az vm extension set -g benchmark --vm-name benchmarkVM2 -n CustomScriptForLinux --publisher Microsoft.OSTCExtensions --version 1.5 --settings '{"fileUris": ["https://raw.githubusercontent.com/NetCoreApps/Benchmarking/profiler/benchmark.sh?v1"],"commandToExecute": "./benchmark.sh '"${INTERNAL_IP}"'"}'
 az vm get-instance-view -n benchmarkVM2 -g benchmark | jq '.instanceView.extensions[] | select(.name == "CustomScriptForLinux").statuses[0].message' | awk -v FS="(---stdout---|---errout---)" '{print $2}' | sed 's/\\n/\'$'\n''/g'
 #Destroy virtual machines
 
