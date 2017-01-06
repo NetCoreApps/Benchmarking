0991fe8fcd12896f650cc23d72be7741a159382f
xplicit <svg@ngs.ru>
2017-01-07 00:34:14 +0700

    Add ormlite to local benchmarking

--- benchmark-local.sh
+++ benchmark-local.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#reset
+wget -qO- http://$1:5051/reset/full &> /dev/null
 #benchmarking
 #warm up
 wrk -c 256 -t 8 -d 10 http://$1:5051/json/reply/hello?name=world > /dev/null
@@ -9,4 +11,12 @@ wrk -c 256 -t 8 -d 5 http://$1:5051/json/reply/hello?name=world | grep Requests
 wrk -c 256 -t 8 -d 5 http://$1:5051/json/reply/hello?name=world | grep Requests
 wrk -c 256 -t 8 -d 5 http://$1:5051/json/reply/hello?name=world | grep Requests
 wrk -c 256 -t 8 -d 5 http://$1:5051/json/reply/hello?name=world | grep Requests
+#benchmarking OrmLite
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json > /dev/null
+echo "Benchmarking .NET Core OrmLite /db"
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
 
