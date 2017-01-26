11da41f0f8bed4a6adca4f35112203f780676576
Sergey Zhukov <svg@ngs.ru>
2017-01-26 21:29:10 +0700

    Add OrmLite to benchmarks

--- benchmark.sh
+++ benchmark.sh
@@ -3,6 +3,8 @@ sudo apt-get update > /dev/null
 sudo apt-get install -y wrk > /dev/null
 
 #benchmarking
+#reset
+wget -qO- http://$1:5051/reset/full &> /dev/null
 #warm up
 wrk -c 256 -t 8 -d 10 http://$1:5051/json/reply/hello?name=world > /dev/null
 #benchmarking
@@ -13,6 +15,13 @@ wrk -c 256 -t 8 -d 5 http://$1:5051/json/reply/hello?name=world | grep Requests
 wrk -c 256 -t 8 -d 5 http://$1:5051/json/reply/hello?name=world | grep Requests
 wrk -c 256 -t 8 -d 5 http://$1:5051/json/reply/hello?name=world | grep Requests
 
+echo "Benchmarking .NET Core /db?format=json"
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1:5051/db?format=json | grep Requests
+
 echo "Benchmarking .NET Core /plaintext"
 wrk -c 256 -t 8 -d 5 http://$1:5051/plaintext | grep Requests
 wrk -c 256 -t 8 -d 5 http://$1:5051/plaintext | grep Requests
@@ -22,12 +31,21 @@ wrk -c 256 -t 8 -d 5 http://$1:5051/plaintext | grep Requests
 
 echo "Benchmarking Mono /json/reply/hello?name=world"
 wget http://$1/json/reply/hello?name=world > /dev/null
+#reset
+wget -qO- http://$1/reset/full &> /dev/null
 wrk -c 50 -t 8 -d 10 http://$1/json/reply/hello?name=world > /dev/null
 wrk -c 50 -t 8 -d 5 http://$1/json/reply/hello?name=world | grep Requests
 wrk -c 50 -t 8 -d 5 http://$1/json/reply/hello?name=world | grep Requests
 wrk -c 50 -t 8 -d 5 http://$1/json/reply/hello?name=world | grep Requests
 wrk -c 50 -t 8 -d 5 http://$1/json/reply/hello?name=world | grep Requests
 wrk -c 50 -t 8 -d 5 http://$1/json/reply/hello?name=world | grep Requests
+
+echo "Benchmarking .NET Core /db?format=json"
+wrk -c 256 -t 8 -d 5 http://$1/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1/db?format=json | grep Requests
+wrk -c 256 -t 8 -d 5 http://$1/db?format=json | grep Requests
 
 echo "Benchmarking Mono /plaintext"
 wrk -c 50 -t 8 -d 10 http://$1/plaintext > /dev/null
